3cc0501e2e45d3f9d8791c4438a1a5c6a07d29f6
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-07-02 12:23:09 +0500

    another update

--- index.js
+++ index.js
@@ -40,16 +40,16 @@ const uploadImg = upload.single("image");//multer({storage: storage}).single('im
 
 const app = express();
 // app.use(cors());
-// app.use(cors({
-//   origin: 'https://plurawlsubscriptions.vercel.app',
-//   // methods: ['GET', 'POST'], // Allow specific HTTP methods
-//   // allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-//   // credentials: true // If you need to send cookies or other credentials
-// }));
+app.use(cors({
+  origin: 'https://plurawlsubscriptions.vercel.app',
+  methods: ['GET', 'POST'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  credentials: true // If you need to send cookies or other credentials
+}));
 
 // app.use(express.json());
 
-app.options('*', cors()); // Preflight requests handling
+// app.options('*', cors()); // Preflight requests handling
 
 
 import db from "./models/index.js";
