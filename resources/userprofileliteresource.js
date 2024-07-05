b645bf22cd1d515366ccb01cb3094be618470bf0
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-07-05 15:19:57 +0500

    user goals

--- resources/userprofileliteresource.js
+++ resources/userprofileliteresource.js
@@ -30,158 +30,11 @@ const UserProfileLiteResource = async (user, currentUser = null) => {
 
 async function getUserData(user, currentUser = null) {
 
-
-    // let checkin = await db.userCheckinModel.findOne({
-    //     where: {
-    //         UserId: user.id,
-    //         type: CheckInTypes.TypeManual
-    //     },
-    //     order: [
-    //         ['createdAt', 'DESC'],
-    //     ],
-    // });
-
-    // let today = moment().format("DD MM YYYY");
-    // let quote = await db.dailyQuoteModel.findOne({
-    //     where: {
-    //         date: today
-    //     }
-    // })
-
-
-    // const originalDate = new Date();
-    // const currentDate = new Date(originalDate.getFullYear(), originalDate.getMonth(), originalDate.getDate());
-
-    // // Calculate the start and end date of the last week
-    // const lastSunday = new Date(currentDate);
-    // ////console.log(`Date Calc${currentDate.getDate()} - ${currentDate.getDay()}`);
-    // lastSunday.setDate(currentDate.getDate() - (currentDate.getDay()));
-    // const lastMonday = new Date(lastSunday);
-    // lastMonday.setDate(lastSunday.getDate() - 6);
-    // //console.log("Last Sunday is ", lastSunday)
-    // //console.log("Last Monday is ", lastMonday)
-
-    // let journals = await getJournalsInAWeek(lastMonday, lastSunday, user.id)
-
-    // // Query to retrieve data for the last week
-    // let checkins = await db.userCheckinModel.findAll({
-    //     where: {
-    //         createdAt: {
-    //             [Op.between]: [lastMonday, lastSunday]
-    //         },
-    //         UserId: user.id
-    //     }
-    // })
-
-    // let sub = await db.subscriptionModel.findOne({
-    //     where: {
-    //         UserId: user.id
-    //     }
-    // })
-    // let plan = null
-    // if(sub){
-    //     let p = JSON.parse(sub.data);
-    //     //console.log("User have subscription plan", p)
-    //     plan = p;
-    // }
-    
-//     let dateSt1 = moment(lastMonday).format("MMM DD")
-//     let dateSt2 = moment(lastSunday).format("MMM DD")
-
-//     var lep = 0
-//     var hep = 0
-//     var leup = 0
-//     var heup = 0
-//     for (let i = 0; i < journals.length; i++) {
-//         let cin = journals[i];
-//         if (cin.mood === CheckinMoods.MoodHep) {
-//             hep = hep + 1
-//         }
-//         if (cin.mood === CheckinMoods.MoodLep) {
-//             lep = lep + 1
-//         }
-//         if (cin.mood === CheckinMoods.MoodLeup) {
-//             leup = leup + 1
-//         }
-//         if (cin.mood === CheckinMoods.MoodHeup) {
-//             heup = heup + 1
-//         }
-//     }
-
-//     for (let i = 0; i < checkins.length; i++) {
-//         let cin = checkins[i];
-//         if (cin.mood === CheckinMoods.MoodHep) {
-//             hep = hep + 1
-//         }
-//         if (cin.mood === CheckinMoods.MoodLep) {
-//             lep = lep + 1
-//         }
-//         if (cin.mood === CheckinMoods.MoodLeup) {
-//             leup = leup + 1
-//         }
-//         if (cin.mood === CheckinMoods.MoodHeup) {
-//             heup = heup + 1
-//         }
-//     }
-
-    
-
-//     var mostCheckedInMood = CheckinMoods.MoodHep;
-//     if (lep > hep && lep > leup && lep > heup) {
-//         mostCheckedInMood = CheckinMoods.MoodLep;
-//     }
-//     else if (leup > hep && leup > lep && leup > heup) {
-//         mostCheckedInMood = CheckinMoods.MoodLeup;
-//     }
-//     else if (heup > hep && heup > lep && heup > leup) {
-//         mostCheckedInMood = CheckinMoods.MoodHeup;
-//     }
-//     let gif = GenerateRandomGif(mostCheckedInMood)
-
-//     let songsCondition = {
-//         createdAt: {
-//             [Op.between]: [lastMonday, lastSunday]
-//         },
-//         UserId: user.id
-//     }
-//     let songs = await db.spotifySongModel.findAll({
-//         where: songsCondition,
-//         limit: 5
-//     })
-
-
-//     var lastWeekVibe = null
-//     if (journals.length > 0 || checkins.length > 0) {
-//         lastWeekVibe = {
-//             checkins: [], journals: journals, startDate: lastMonday, endDate: lastSunday, mostCheckedInMood: mostCheckedInMood,
-//             lep: lep, hep: hep, leup: leup, heup: heup, dateString: dateSt1 + " - " + dateSt2, gif: gif, tracks: songs
-//         }
-
-//         let year = moment(lastSunday).format("YYYY");
-//         ////console.log(`FInding ${year} ${dateSt2} ${dateSt1}`)
-//         let snapshot = await db.weeklySnapshotModel.findOne({
-//             where: {
-//                 sunday: dateSt2,
-//                 monday: dateSt1,
-//                 year: year,
-//                 UserId: user.id,
-//             }
-//         })
-//         lastWeekVibe.snapshot = snapshot;
-//     }
-
-// let haveJournals = false
-// let myJournals = await db.userJournalModel.findAll({
-//     where: {
-//         UserId: user.id
-//     }
-// })
-// ////console.log("Have Journals ", myJournals)
-// if(myJournals){
-//     if(myJournals.length > 0){
-//         haveJournals = true;
-//     }
-// }
+    let goals = await db.userGoalModel.findAll({
+        where: {
+            UserId: user.id
+        }
+    })
 
 
 let level = 0
@@ -218,25 +71,9 @@ const dailyLoginData = await db.dailyLogin.findOne({
         city: user.city,
         color: getRandomColor(),
         lastLogin: lastLogin,
-        // company: user.company,
-        // industry: user.industry,
-        // title: user.title,
-        // race: user.race,
-        // lgbtq: user.lgbtq,
-        // gender: user.gender,
-        // veteran: user.veteran,
-        // provider_id: user.provider_id,
-        // provider_name: user.provider_name,
-        // points: user.points,
-        // lastcheckin: checkin,
-        // lastWeekVibe: lastWeekVibe,
-        // quote_of_day: quote,
-        // have_journals: haveJournals,
-        // pronouns: user.pronouns,
-        // countries: (user.countries !== null && user.countries != "") ? JSON.parse(user.countries) : "",
-        // dob: user.dob,
         points: user.points,
         level: level,
+        goals: goals,
         // plan: plan,
     }
 
