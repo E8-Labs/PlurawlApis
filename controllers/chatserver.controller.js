f9eb669fba71a56d9ff27b9ecb435374ea0035b3
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-10-16 17:25:21 +0500

    an update

--- controllers/chatserver.controller.js
+++ controllers/chatserver.controller.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { Server } from "socket.io";
-import { OpenAI } from "openai"; // Adjust import based on your OpenAI SDK version
+// import { OpenAI } from "openai"; // Adjust import based on your OpenAI SDK version
 import http from "http"; // Importing http module
 
 import dotenv from "dotenv";
@@ -9,28 +9,29 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const openai = new OpenAI({
-  apiKey: process.env.AIKey,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.AIKey,
+// });
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
   socket.on("sendMessage", async (message) => {
-    const responseStream = openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: message }],
-      stream: true,
-    });
+    console.log("Message received ", message);
+    // const responseStream = openai.chat.completions.create({
+    //   model: "gpt-3.5-turbo",
+    //   messages: [{ role: "user", content: message }],
+    //   stream: true,
+    // });
 
-    responseStream.on("data", (chunk) => {
-      const chunkData = chunk.choices[0].delta.content; // Extract the content from the chunk
-      socket.emit("receiveMessage", chunkData); // Emit the chunk back to the client
-    });
+    // responseStream.on("data", (chunk) => {
+    //   const chunkData = chunk.choices[0].delta.content; // Extract the content from the chunk
+    //   socket.emit("receiveMessage", chunkData); // Emit the chunk back to the client
+    // });
 
-    responseStream.on("end", () => {
-      socket.emit("receiveMessage", ""); // Emit an empty string to signify end of message
-    });
+    // responseStream.on("end", () => {
+    //   socket.emit("receiveMessage", ""); // Emit an empty string to signify end of message
+    // });
   });
 
   socket.on("disconnect", () => {
